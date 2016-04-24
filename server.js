0e6d3152d8a4622f6ba225c8835d03970cbd9396
z4nime <boybrandla@hotmail.com>
2016-04-24 14:25:33 +0700

    first mission sucess

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
+var request = require('request')
+
+var token = "CAATqXXHDbL8BAG4dU7BBZCRBIngWE92ZB5z4D6lNonQhrUrFTtKQLpCDIYTg13f0TDf40cIpOUn4ZBvvnfLzKCMkHFbRQgu54ry5XA1XZAuplMt8dBzEaht2wlreuZAhZAJFERa0t61mq0fGaJ5pL8Wsf1ZCEivsDXyv2U5gAXo3QqTfsR14ZBGgXm6hXTSDWWADZAuB5GJLYXwZDZD";
+
+function sendTextMessage(sender, text) {
+  messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
 
 app.use(bodyParser.json())
 
@@ -26,6 +49,7 @@ app.post('/webhook/', function (req, res) {
       text = event.message.text;
       // Handle a text message from this sender
       console.log(text);
+      sendTextMessage(sender,text);
     }
   }
   res.sendStatus(200);
