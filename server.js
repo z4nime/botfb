37486af5ff0d092842fbe1f0d0aa09c5bf86f7e9
z4nime <boybrandla@hotmail.com>
2016-04-24 14:00:53 +0700

    add webhook

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+
+app.get('/webhook', function (req, res) {
+  if (req.query['hub.verify_token'] === <YOUR_VERIFY_TOKEN>) {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.send('Error, wrong validation token');    
+  }
+});
+
+
 app.set('port',(process.env.PORT || 3000))
 app.listen(app.get('port'), function () {
   console.log('Example app listening on port ' + app.get('port'));
